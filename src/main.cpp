be26243d5cfd4702a843e152fd24275d1e1cfaf0
charliedavidson <charliedavidson658@gmail.com>
2022-06-26 18:57:49 -0400

    Render triangle (with GLUT)

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,67 @@
-#include <GLFW/glfw3.h>
-#include <cstdio>
+/**********************************************************************
 
-static void error_callback(int error, const char* description) {
-    fprintf(stderr, "Error: %s\n", description);
+  OpenGL-Introduction
+
+  June, 7th, 2000
+
+  This tutorial was written by Philipp Crocoll
+  Contact: 
+	philipp.crocoll@web.de
+	www.codecolony.de
+
+  Every comment would be appreciated.
+
+  If you want to use parts of any code of mine:
+	let me know and
+	use it!
+
+***********************************************************************/
+
+#include <GLUT/glut.h>
+
+
+void Display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    glBegin(GL_POLYGON);
+    glColor3f(0.0,1.0,0.0);
+    glVertex3f(-0.5,-0.5,-3.0);
+    glColor3f(1.0,0.0,0.0);
+    glVertex3f(0.5,-0.5,-3.0);
+    glColor3f(0.0,0.0,1.0);
+    glVertex3f(0.5,0.5,-3.0);
+    glEnd();
+    glFlush();			//Finish rendering
 }
 
-int main()
+void Reshape(int x, int y)
 {
-    GLFWwindow* window;
+    if (y == 0 || x == 0) return;  //Nothing is visible then, so return
+    //Set a new projection matrix
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //Angle of view:40 degrees
+    //Near clipping plane distance: 0.5
+    //Far clipping plane distance: 20.0
+    gluPerspective(40.0,(GLdouble)x/(GLdouble)y,0.5,20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glViewport(0,0,x,y);  //Use the whole window for rendering
+}
 
-    glfwSetErrorCallback(error_callback);
-
-    if (!glfwInit()) {
-        return -1;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    window = glfwCreateWindow(640, 480, "My Window", nullptr, nullptr);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    while(!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+int main (int argc, char **argv)
+{
+    //Initialize GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(300,300);
+    //Create a window with rendering context and everything else we need
+    glutCreateWindow("Intro");
+    glClearColor(0.0,0.0,0.0,0.0);
+    //Assign the two used Msg-routines
+    glutDisplayFunc(Display);
+    glutReshapeFunc(Reshape);
+    //Let GLUT get the msgs
+    glutMainLoop();
     return 0;
 }
